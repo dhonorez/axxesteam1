8dfb86877ae30f65ed78d94189d721b68d1e3348
Dylan Honorez <dylan.honorez@skryv.com>
2017-02-23 20:57:12 +0100

    Solution?

---
+++ src/main/java/com/axxes/alg2/Calculator.java
@@ -0,0 +1,78 @@
+package com.axxes.alg2;
+
+import com.axxes.model.Cache;
+import com.axxes.model.Endpoint;
+import com.axxes.model.Video;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by dylanhonorez on 23/02/17.
+ */
+public class Calculator {
+
+    private Set<Videoset> videosets = new HashSet<>();
+    Set<Cache> cacheSet;
+    Set<Endpoint> endpoints;
+    Set<Video> videos;
+
+    public void calculate() {
+        Cache cache = cacheSet.iterator().next();
+        calculateVideoSets(cache, videos);
+
+        System.out.println("Calculated videosets: " + videosets.size());
+        calculateScores(cache, endpoints, videos);
+
+        getHighestScore(cache);
+    }
+
+    private void getHighestScore(Cache cache) {
+        Videoset videoset = videosets.stream().sorted(Comparator.comparingInt(Videoset::getScore).reversed()).findFirst().get();
+        System.out.println(videoset);
+    }
+
+    private void calculateScores(Cache cache, Set<Endpoint> endpoints, Set<Video> videos) {
+        for (Video v: videos) {
+            for (Videoset set : videosets) {
+                if (set.getVideos().contains(v)) {
+                    for (Endpoint endpoint : v.getPopularity().keySet()) {
+                        set.addScore((endpoint.getLatencyDataCenter() - endpoint.getLatencyToCache().get(cache)) * v.getPopularity().get(endpoint);
+                    }
+                }
+            }
+        }
+    }
+
+    private void calculateVideoSets(Cache cache, Set<Video> videos) {
+        for (Video v : videos) {
+            Videoset videoset = new Videoset();
+
+            if (videoset.canAddVideo(v)) {
+                videoset.addVideo(v);
+                Set<Video> clone = new HashSet<>(videos);
+                clone.remove(v);
+                calculateForVideoSet(videoset, clone);
+            } else {
+                videosets.add(videoset);
+            }
+        }
+    }
+
+    private void calculateForVideoSet(Videoset previous, Set<Video> videos) {
+        for (Video v : videos) {
+            Videoset videoset = new Videoset(previous);
+            if (videoset.canAddVideo(v)) {
+                videoset.addVideo(v);
+                Set<Video> clone = new HashSet<>(videos);
+                clone.remove(v);
+                calculateForVideoSet(videoset, clone);
+            } else {
+                videosets.add(videoset);
+            }
+        }
+    }
+
+
+}
