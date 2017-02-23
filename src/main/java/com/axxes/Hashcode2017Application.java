e5a07c76afb46e0bb21b29b9065516dec1067c3c
Dylan Honorez <dylan.honorez@skryv.com>
2017-02-23 17:45:33 +0100

    Initial maven commit.

---
+++ src/main/java/com/axxes/Hashcode2017Application.java
@@ -0,0 +1,12 @@
+package com.axxes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hashcode2017Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hashcode2017Application.class, args);
+	}
+}
