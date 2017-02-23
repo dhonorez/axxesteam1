03391fbde16b2bafbaf1a9facba6f20120c502f7
Dylan Honorez <dylan.honorez@skryv.com>
2017-02-23 20:16:03 +0100

    Reader it 1

--- src/main/java/com/axxes/model/Video.java
+++ src/main/java/com/axxes/model/Video.java
@@ -9,6 +9,6 @@ public class Video {
 
     private final int id;
     private final int size;
-    private final Map<Endpoint, Integer> popularity; // endpoint, views
+    private Map<Endpoint, Integer> popularity; // endpoint, views
 
 }
